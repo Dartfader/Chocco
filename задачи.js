10eea6ced0a71ae5dda1158c1c0a4a5e0a850ec0
VLD <dartwavve@gmail.com>
2020-03-22 12:51:10 +0400

    меню

---
+++ задачи.js
@@ -0,0 +1,43 @@
+// var name="Vlad"
+// console.log(name);
+// name="Oleg"
+// console.log(name);
+
+// var n=2
+// if (n>10) {
+//     console.log(n);
+// }
+// else {
+//     console.log("ошибка")
+// }
+
+// for (let i=0;i<10;i++) {
+//     console.log(i);
+// }
+
+
+//
+// function sum(p1, p2, p3) {
+//     var result=p1+p2+p3;
+//
+//     return result;
+// }
+//
+// var h=sum(10, 20, 30);
+// console.log(h);
+//
+// var s=sum(1,2,3);
+// console.log(s);
+//
+// for (let i=0;i<5;i++) {
+//     console.log(i);
+// }
+
+// var mass= ['Привет', 'Sergey'] ;
+// mass.push('я изучаю','javascript');
+// console.log(mass.length);
+//
+// for(let i=0;i<mass.length;i++) {
+//     console.log(mass[i]);
+// }
+
