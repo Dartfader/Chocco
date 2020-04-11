188e4a30f7492620e3efe07bebaa733c4b9da720
VLD@DESKTOP-11KCMD7 <dartwavve@gmail.com>
2020-04-11 17:01:46 +0400

    work menu

--- menu.js
+++ menu.js
@@ -1,18 +1,20 @@
 const menuList=document.querySelector('.menu__list');
 const menuItem=document.querySelectorAll('.menu__item');
+const menuLink=document.querySelectorAll('.menu__link');
+const close=document.querySelector('.menu__item__close');
 
  menuList.addEventListener('click', function(e)  {
      let target = e.target;
+     console.log('нажатие на target'+ target);
      let currentItem=target.closest(".menu__item");//делегирование
      e.preventDefault();
-     if (target.classList.contains("menu__link")) {
+     if (target.classList.contains("menu__item__title__text")||target.classList.contains("menu__link")) {
             console.log(currentItem);
          e.preventDefault();
          if (!currentItem.classList.contains("menu__active")) {
              for(let i=0;i<menuItem.length;i++) {
                  menuItem[i].classList.remove("menu__active")
                  // console.log(item);
-
              }
              currentItem.classList.add("menu__active");
          }
@@ -23,3 +25,16 @@ const menuItem=document.querySelectorAll('.menu__item');
      }
 
  });
+
+ menuLink.onclick= function (e) {
+     console.log(menuLink);
+ };
+
+ close.addEventListener('click',function (e) {
+        let closeTarget=e.target;
+        let currentClose=closeTarget.closest("menu__item__close");
+        if(closeTarget.classList.contains("menu__item__close")) {
+
+        }
+
+ })
