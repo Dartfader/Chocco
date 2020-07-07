239e0444de38ba884ef71d7a60f647a6320f7ea9
VLD@DESKTOP-11KCMD7 <dartwavve@gmail.com>
2020-07-08 00:00:27 +0400

    Updates

--- gulp.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    SRC_PATH : 'src',
-    DIST_PATH : 'dist',
-    STYLE_LIBS : ['node_modules/normalize.css/normalize.css'],
-    JS_LIBS : ['node_modules/jquery/dist/jquery.js']
-}
